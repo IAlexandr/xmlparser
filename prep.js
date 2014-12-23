4fe973ca7ddd0540995edf4cac3c8bd92e63056d
Alexandr Ivanov <aivanov@informatica.ru>
2014-12-23 16:48:48 +0300

    подготовка и приведение вида выходных данных к схеме

--- prep.js
+++ prep.js
@@ -1,59 +1,81 @@
-
 var _ = require('underscore');
+var fcFields = ['Parcels', 'Zones', 'Boundaries'];
 
 var prepFeature = function (data) {
-    var feature = {};
-    data = _.pick(data, 'CadastralNumber', 'DateCreated','Name', 'State', 'Entity_Spatial');
+    var feature = {
+        type: 'Feature',
+        properties: {}
+    };
     _.each(data, function (v, k) {
-        feature[k] = v;
+
+        if (k === 'Entity_Spatial') {
+            var geoJSON = prepGeometry('Polygon', v.Spatial_Element.Spelement_Unit);
+            feature.geometry = geoJSON;
+        } else {
+            feature.properties[k] = v;
+        }
     });
-    //todo
+    return feature;
 };
 
-var prepParcels = function (data) {
-    var result = [];
-    //todo преобразовать в geoJSON
+var prepFeatureCollection = function (data) {
+    var featureCollection = {
+        type: 'FeatureCollection',
+        features: []
+    };
     data = prepArrs(data);
-    _.each(data, function (parcel) {
-        result.push(prepFeature(parcel));
+    _.each(data, function (feature) {
+        featureCollection.features.push(prepFeature(feature));
     });
-    return result;
+    return featureCollection;
 };
 
 var prepArrs = function (data) {
     var result = [];
-    _.each(data, function (v, k) {
+    _.each(data, function (v) {
         result.push(prepObject(v));
     });
     return result;
+};
+
+var hz = function (k, v) {
+    var obj;
+    if (_.contains(fcFields, k)) {
+
+        if (Array.isArray(v)) {
+            var valKeys = Object.keys(v[0]);
+            obj = prepFeatureCollection(v[0][valKeys[0]]);
+        } else {
+            obj = [prepFeatureCollection(v)];
+        }
+    } else {
+        if (Array.isArray(v)) {
+            if (v.length > 1) {
+                obj = prepArrs(v);
+            } else {
+                obj = prepObject(v[0]);
+            }
+        } else {
+            if (typeof v === "object") {
+                obj = prepObject(v);
+            } else {
+                obj = v;
+            }
+        }
+    }
+    return obj;
 };
 
 var prepObject = function (data) {
     var obj = {};
     if (typeof data === "object") {
         _.each(data, function (v, k) {
-            if (k === "Parcel") {
-                obj[k] = prepParcels(v);
+            if (k === '$') {
+                _.each(v, function (val, key) {
+                    obj[key] = prepObject(val);
+                });
             } else {
-                if (Array.isArray(v)) {
-                    if (v.length > 1) {
-                        obj[k] = prepArrs(v);
-                    } else {
-                        obj[k] = prepObject(v[0]);
-                    }
-                } else {
-                    if (k === "$") {
-                        _.each(v, function (val, key) {
-                            obj[key] = prepObject(val);
-                        });
-                    } else {
-                        if (typeof v === "object") {
-                            obj[k] = prepObject(v);
-                        } else {
-                            obj[k] = v;
-                        }
-                    }
-                }
+                obj[k] = hz(k, v);
             }
         });
         return obj;
@@ -62,34 +84,31 @@ var prepObject = function (data) {
     }
 };
 
-var prepGeoJSON = function (data) {
-    var geoJSON = {
-        "type": "Polygon",
+var prepGeometry = function (type, data) {
+
+    var geometry = {
+        "type": type,
         "coordinates": []
     };
-    
-    _.each(data, function (point) {
-        geoJSON.coordinates.push([point.Ordinate.X,point.Ordinate.Y]);
-    });
-    return geoJSON;
+
+    switch (type) {
+        case 'Polygon':
+            _.each(data, function (point) {
+                geometry.coordinates.push([point.Ordinate.X, point.Ordinate.Y]);
+            });
+            return geometry;
+            break;
+        default:
+            //todo
+            return geometry;
+            break;
+    }
 };
 
-function formatDate(date) {
-    var dd = date.getDate();
-    if ( dd < 10 ) dd = '0' + dd;
-    var mm = date.getMonth()+1;
-    if ( mm < 10 ) mm = '0' + mm;
-    var yyyy = date.getFullYear(); // % 100
-    //if ( yy < 10 ) yy = '0' + yy;
-    return yyyy + "-" + mm + '-' + dd;
-}
-
 var prepCadBlock = function (data) {
-    var cadBlock = {};
-
-    cadBlock["cadNumber"] = data.Cadastral_Block.CadastralNumber;
-    cadBlock["date"] = formatDate(new Date());//data.Certification_Doc.Date;
-    cadBlock["border"] = prepGeoJSON(data.Cadastral_Block.SpatialData.Entity_Spatial.Spatial_Element.Spelement_Unit);
+    var cadBlock = data.Cadastral_Block;
+    cadBlock["SpatialData"] = prepFeature(cadBlock.SpatialData);
+    cadBlock["Author"] = data.Certification_Doc;
     return cadBlock;
 };
 
