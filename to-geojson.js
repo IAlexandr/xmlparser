87e5b9985ec2b216d9eec1a0ac93653d697f2c72
Alexandr Ivanov <aivanov@informatica.ru>
2015-06-08 17:00:06 +0300

    fix wrong build geojson

--- to-geojson.js
+++ to-geojson.js
@@ -8,16 +8,27 @@ module.exports = function (data) {
             var fe = feature.properties.EntitySpatial;
             if (fe) {
                 if (fe.SpatialElement) {
-                    var arr = [];
+                    var arr0 = [];
+                    var arr1 = [];
                     if (Array.isArray(fe.SpatialElement)) {
-                        arr = fe.SpatialElement[0].SpelementUnit;
+                        arr0 = fe.SpatialElement[0].SpelementUnit;
+                        arr1 = fe.SpatialElement[1].SpelementUnit;
                         // todo скорее всего [0] полигоны участков без дырок, [1] дырки в полигоне
                     } else {
-                        arr = fe.SpatialElement.SpelementUnit;
+                        arr0 = fe.SpatialElement.SpelementUnit;
                     }
-                    arr.forEach(function (objWithCoord) {
-                        feature.geometry.coordinates.push([objWithCoord.Ordinate.Y, objWithCoord.Ordinate.X]);
+                    var arr0result = [];
+                    var arr1result = [];
+                    arr0.forEach(function (objWithCoord) {
+                        arr0result.push([parseFloat(objWithCoord.Ordinate.Y), parseFloat(objWithCoord.Ordinate.X)]);
                     });
+                    feature.geometry.coordinates.push(arr0result);
+                    if (arr1.length > 0) {
+                        arr1.forEach(function (objWithCoord) {
+                            arr1result.push([parseFloat(objWithCoord.Ordinate.Y), parseFloat(objWithCoord.Ordinate.X)]);
+                        });
+                        feature.geometry.coordinates.push(arr1result);
+                    }
                 }
                 delete feature.properties['EntitySpatial'];
             }
